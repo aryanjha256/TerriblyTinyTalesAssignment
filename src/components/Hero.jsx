654b354e6d7eaa700edd105a8b88b589bf872f98
Aryan Kumar <aryanajha256@gmail.com>
2023-11-13 19:04:02 +0530

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,42 +2,68 @@ import cover from "../assets/cover.jpg";
 import headshot from "../assets/headshot.jpg";
 import { user } from "../data/info.json";
 
+import { FaEye, FaHeart, FaStar, FaThumbsUp } from "react-icons/fa";
+
 const Hero = () => {
   return (
-    <div>
+    <>
       <div className="bg-gradient-to-bl h-3 bg-yellow-200"></div>
-      <img
-        className="max-h-56 w-full object-cover object-top"
-        src={cover}
-        alt="Cover Image"
-      />
-      <div className="max-w-4xl m-auto flex">
+      <div className="max-w-4xl m-auto">
         <img
-          className="relative max-h-44 rounded-full -mt-12 border-4 border-white"
-          src={headshot}
-          alt="Profile Picture"
+          className="max-h-56 w-full object-cover object-top"
+          src={cover}
+          alt="Cover Image"
         />
-        <div className="ml-4">
-          <div className="font-bold">{user.name}</div>
-          <div className="text-gray-500">@{user.username}</div>
-          <div className="text-gray-600">
-            <span>{user.followers}</span>
-            <span>Followers</span>
-            <span>{user.following}</span>
-            <span>Following</span>
+        <div className="flex px-6">
+          <img
+            className="relative max-h-44 rounded-full -mt-12 border-2 border-gray-300"
+            src={headshot}
+            alt="Profile Picture"
+          />
+          <div className="ml-4 mt-2">
+            <div className="font-bold text-lg">{user.name}</div>
+            <div className="text-gray-500 -mt-1">@{user.username}</div>
+            <div className="text-gray-600 flex space-x-4 mt-4">
+              <div className="flex flex-col items-center min-w-[72px]">
+                <div className="px-4 py-1 border w-full text-center font-extrabold">
+                  {user.followers}
+                </div>
+                <div className="text-sm">Followers</div>
+              </div>
+              <div className="flex flex-col items-center min-w-[72px]">
+                <div className="px-4 py-1 border w-full text-center font-extrabold">
+                  {user.following}
+                </div>
+                <div className="text-sm">Following</div>
+              </div>
+            </div>
           </div>
-          <div>
-            <div>{user.bio}</div>
-            <a
-              href="https://github.com/aryanjha256"
-              className="text-teal-300 hover:text-teal-400"
-            >
-              GitHub
-            </a>
+        </div>
+        <div className="mt-6 text-gray-700 px-6">
+          <div className="text-lg">{user.bio}</div>
+          <a
+            href="https://github.com/aryanjha256"
+            className="text-teal-300 hover:text-teal-400 text-sm font-semibold"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="flex items-center space-x-4 mt-6 px-6">
+          <div className="flex items-center space-x-2">
+            <FaStar fill="teal" /> <span className="text-xs">256</span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FaThumbsUp fill="yellow" /> <span className="text-xs">34</span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FaEye fill="gray" /> <span className="text-xs">12.13K</span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FaHeart fill="red" /> <span className="text-xs">3.7K</span>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
